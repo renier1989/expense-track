1c43237254ee352977dcf3a147e2dd1a9d7fb3d5
Renier Vargas <renier1989@gmail.com>
2023-08-28 08:47:06 -0500

    Class106. Añadiendo el Formulario de Gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,22 +1,52 @@
 /* eslint-disable react/prop-types */
-import closeModalIcon from '../img/cerrar.svg'
-const Modal = ({setModal, animateModal, setAnimateModal}) => {
-    const closeModal = () => {
-        setAnimateModal(false)
-        setTimeout(() => {
-            setModal(false);
-        }, 500);
-    }
+import closeModalIcon from "../img/cerrar.svg";
+const Modal = ({ setModal, animateModal, setAnimateModal }) => {
+  const closeModal = () => {
+    setAnimateModal(false);
+    setTimeout(() => {
+      setModal(false);
+    }, 500);
+  };
   return (
     <div className="modal">
-        <div className="cerrar-modal">
-            <img onClick={closeModal} src={closeModalIcon} alt="close-modal-icon" />
+      <div className="cerrar-modal">
+        <img onClick={closeModal} src={closeModalIcon} alt="close-modal-icon" />
+      </div>
+      <form className={`formulario ${animateModal ? "animar" : "cerrar"}`}>
+        <legend>New Expense</legend>
+        <div className="campo">
+          <label htmlFor="expense_name">Expense Name</label>
+          <input
+            id="expense_name"
+            type="text"
+            placeholder="Entre the Expense Name"
+          />
         </div>
-        <form className={`formulario ${animateModal ? 'animar':'cerrar'}`}>
-            <legend>New Expense</legend>
-        </form>
+        <div className="campo">
+          <label htmlFor="amount">Amount</label>
+          <input
+            id="amount"
+            type="text"
+            placeholder="Amount of the Expense Ex: $1000"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="amount">Category</label>
+          <select name="" id="category">
+            <option value="">-- Select --</option>
+            <option value="save">Save</option>
+            <option value="food">Food</option>
+            <option value="home">Home</option>
+            <option value="misc_expenses">Misc. Expenses</option>
+            <option value="leisure">Leisure</option>
+            <option value="health">Health</option>
+            <option value="subscriptions">Subscriptions</option>
+          </select>
+        </div>
+        <input type="submit" value="Add Expense" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
