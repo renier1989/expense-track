3f8fb9095b6ca0313984f3d37e15ce2218d74670
Renier Vargas <renier1989@gmail.com>
2023-08-28 17:19:02 -0500

    Class124. Calcular el Porcentaje gastado para la gráfica Circular

--- src/components/PlanBudget.jsx
+++ src/components/PlanBudget.jsx
@@ -5,20 +5,25 @@ import { formatCurrency } from "../helpers";
 
 /* eslint-disable react/prop-types */
 const PlanBudget = ({presupuesto, gastos}) => {
+    const [porcentaje , setPorcentaje] = useState(0);
     const [disponible , setDisponible] = useState(0);
     const [gastado , setGastado] = useState(0);
     useEffect(() => {
         const totalGastado = gastos.reduce((total, gasto)=> gasto.cantidad + total  , 0 );
         const totalDisponible = presupuesto - totalGastado;
+        const nuevoPorcentaje = (((presupuesto - totalDisponible) / presupuesto) * 100).toFixed(2);
         setDisponible(totalDisponible);
         setGastado(totalGastado);
+        setTimeout(() => {
+            setPorcentaje(nuevoPorcentaje);
+        }, 1000);
     }, [gastos]);
 
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
         <CircularProgressbar
-            value={50}
+            value={porcentaje}
         />
       </div>
       <div className="contenido-presupuesto">
