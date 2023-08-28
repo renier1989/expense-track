5a0cd4a28b8064f8332f71fc10d38713f7e3effe
Renier Vargas <renier1989@gmail.com>
2023-08-28 17:25:35 -0500

    ClassDetener 126. Mostrando Textos en Gráfica Circular

--- src/components/PlanBudget.jsx
+++ src/components/PlanBudget.jsx
@@ -25,9 +25,11 @@ const PlanBudget = ({presupuesto, gastos}) => {
         <CircularProgressbar
         styles={buildStyles({
             pathColor: '#3B82F6',
-            trailColor: '#F5F5F5'
+            trailColor: '#F5F5F5',
+            textColor: '#3B82F6',
         })}
             value={porcentaje}
+            text={`${porcentaje}% Spent`}
         />
       </div>
       <div className="contenido-presupuesto">
