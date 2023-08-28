cecc2a65b6ae07d328a3b844d98ee91f318e1f96
Renier Vargas <renier1989@gmail.com>
2023-08-28 09:59:56 -0500

    Class109. Creando una función para generar un ID único y Añadiendo Gastos al State

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { useState } from "react";
 import Header from "./components/Header";
 import newExpenseIcon from "./img/nuevo-gasto.svg";
 import Modal from "./components/Modal";
+import {generarId} from "./helpers"
+
+
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidBudget, setIsValidBudget] = useState(false);
   const [modal, setModal] = useState(false);
   const [animateModal, setAnimateModal] = useState(false);
-  const [gastos, setGastos] = useState({});
+  const [gastos, setGastos] = useState([]);
 
   const handleModal = () => {
     setModal(true);
@@ -18,8 +21,13 @@ function App() {
   };
 
   const  setExpenses = expenses => {
-    
-    console.log(expenses);
+    expenses.id = generarId();
+    setGastos([...gastos,expenses]);
+    setAnimateModal(false);
+    setTimeout(() => {
+      setModal(false);
+    }, 500);
+    // console.log(expenses);
   }
 
   return (
